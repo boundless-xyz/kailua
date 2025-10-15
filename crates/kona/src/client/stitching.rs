b1e4e45c463d2efc9c66962235c176e0f08529e0
Rami Khalil <the.rami.khalil@gmail.com>
2025-10-15 12:27:44 +0300

    fixes

--- crates/kona/src/client/stitching.rs
+++ crates/kona/src/client/stitching.rs
@@ -568,7 +568,7 @@ pub mod tests {
     use std::iter::repeat_n;
 
     fn setup() {
-        kona_cli::LogConfig::new(kona_cli::LogArgs {
+        let _ = kona_cli::LogConfig::new(kona_cli::LogArgs {
             level: 1,
             stdout_quiet: false,
             stdout_format: Default::default(),
@@ -576,12 +576,11 @@ pub mod tests {
             file_format: Default::default(),
             file_rotation: Default::default(),
         })
-        .init_tracing_subscriber(None)
-        .unwrap();
+        .init_tracing_subscriber(None);
     }
 
     fn teardown() {
-        kona_cli::LogConfig::new(kona_cli::LogArgs {
+        let _ = kona_cli::LogConfig::new(kona_cli::LogArgs {
             level: 0,
             stdout_quiet: false,
             stdout_format: Default::default(),
@@ -589,8 +588,7 @@ pub mod tests {
             file_format: Default::default(),
             file_rotation: Default::default(),
         })
-        .init_tracing_subscriber(None)
-        .unwrap();
+        .init_tracing_subscriber(None);
     }
 
     fn validate_proof_journal(
