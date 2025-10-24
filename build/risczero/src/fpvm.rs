28407b650c2ac8b8e8f6361e6d76b5fa4d6a626c
Rami Khalil <the.rami.khalil@gmail.com>
2025-10-24 15:14:14 +0300

    rebuild FPVM

--- build/risczero/src/fpvm.rs
+++ build/risczero/src/fpvm.rs
@@ -1,4 +1,4 @@
 
 pub const KAILUA_FPVM_KONA_ELF: &[u8] = include_bytes!("kailua-fpvm-kona.bin");
 pub const KAILUA_FPVM_KONA_PATH: &str = "./kailua-fpvm-kona.bin";
-pub const KAILUA_FPVM_KONA_ID: [u32; 8] = [0x2904EB97, 0x55867EA7, 0x38CCEB23, 0xDFD0FD7C, 0x30854006, 0x90D7C9B6, 0x74B0A4CE, 0xFFFEF6D5];
+pub const KAILUA_FPVM_KONA_ID: [u32; 8] = [0x6AEC2069, 0xAFCAE378, 0x7542D6D7, 0x4C09734E, 0xFE05A5C7, 0x6B69844A, 0x1853D109, 0xB5050E04];
