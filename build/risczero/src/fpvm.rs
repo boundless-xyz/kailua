de6b07ec6b6744e1ea21b47fd6c71762beb363f1
Rami Khalil <the.rami.khalil@gmail.com>
2026-04-29 13:12:47 +0300

    rebake debug elf

--- build/risczero/src/fpvm.rs
+++ build/risczero/src/fpvm.rs
@@ -1,18 +1,18 @@
 
 pub const KAILUA_FPVM_KONA_ELF: &[u8] = include_bytes!("bin/kailua-fpvm-kona.bin");
 pub const KAILUA_FPVM_KONA_PATH: &str = "bin/kailua-fpvm-kona.bin";
-pub const KAILUA_FPVM_KONA_ID: [u32; 8] = [0x70F0D98D, 0xF854BB86, 0x95E5E86F, 0x82D24CED, 0x414324AA, 0x36521E2B, 0x8AAABD3, 0x8BCFCF5];
+pub const KAILUA_FPVM_KONA_ID: [u32; 8] = [0x87E73F7A, 0xF608E82A, 0xCCAFACF6, 0x8CB1AAA3, 0x96AB196B, 0x4719EC4C, 0x50855960, 0xE55F56C3];
 
 #[cfg(feature = "eigen")]
 pub const KAILUA_FPVM_HOKULEA_ELF: &[u8] = include_bytes!("bin/kailua-fpvm-hokulea.bin");
 #[cfg(feature = "eigen")]
 pub const KAILUA_FPVM_HOKULEA_PATH: &str = "bin/kailua-fpvm-hokulea.bin";
 #[cfg(feature = "eigen")]
-pub const KAILUA_FPVM_HOKULEA_ID: [u32; 8] = [0xE62DFFD0, 0x53CCFDE9, 0x2A765A45, 0x5C9793CC, 0x89CEED39, 0x8F251EB4, 0x11F0AF86, 0xDE0A3459];
+pub const KAILUA_FPVM_HOKULEA_ID: [u32; 8] = [0xB83F89D8, 0x802A0050, 0x67BFA90E, 0x66D8F856, 0x67854DCC, 0xB7515E3, 0x376141A3, 0x24E891B4];
 
 #[cfg(feature = "celestia")]
 pub const KAILUA_FPVM_HANA_ELF: &[u8] = include_bytes!("bin/kailua-fpvm-hana.bin");
 #[cfg(feature = "celestia")]
 pub const KAILUA_FPVM_HANA_PATH: &str = "bin/kailua-fpvm-hana.bin";
 #[cfg(feature = "celestia")]
-pub const KAILUA_FPVM_HANA_ID: [u32; 8] = [0xA7E72B29, 0xE5C58DBA, 0xB3417077, 0xA2F8E253, 0x157FDB9E, 0x1FDBAFEC, 0xA367A5A7, 0x18A47B7D];
+pub const KAILUA_FPVM_HANA_ID: [u32; 8] = [0xD8208090, 0xD30F4B8, 0x2F70570, 0x9F084A4C, 0x2846125A, 0x12703AF, 0x3489B478, 0x293B8D7D];
